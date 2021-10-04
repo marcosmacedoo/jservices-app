b0467d4601c84031844733631fe1b18d49671d05
Marcos Macedo <marcosmacedoo.dev@gmail.com>
2021-10-04 19:42:33 -0300

    refactor: uncounpling ModalRegistrationService of App

--- js/App.js
+++ js/App.js
@@ -1,31 +1,11 @@
-import dataIds from './dataIds.js'
 import { ModalRegistrationService } from './ModalRegistrationService.js'
 
 function App() {
-    const $buttonOpenModalRegistrationService = document.querySelector(
-        `[data-id=${dataIds.buttonOpenModalRegistrationService}]`
-    )
-    const $buttonCloseModalRegistrationService = document.querySelector(
-        `[data-id=${dataIds.buttonCloseModalRegistrationService}]`
-    )
     const modalRegistrationService = ModalRegistrationService()
-
-    function listeningClickButtonOpenModalRegistrationService() {
-        $buttonOpenModalRegistrationService.addEventListener('click', () =>
-            modalRegistrationService.open()
-        )
-    }
-
-    function listeningCloseButtonModalRegistrationService() {
-        $buttonCloseModalRegistrationService.addEventListener('click', () =>
-            modalRegistrationService.close()
-        )
-    }
 
     return {
         run: () => {
-            listeningClickButtonOpenModalRegistrationService()
-            listeningCloseButtonModalRegistrationService()
+            modalRegistrationService.listeningEventsClicks()
         },
     }
 }
--- js/ModalRegistrationService.js
+++ js/ModalRegistrationService.js
@@ -4,14 +4,34 @@ export function ModalRegistrationService() {
     const $modal = document.querySelector(
         `[data-id=${dataIds.modalRegistrationService}]`
     )
+    const $buttonOpenModalRegistrationService = document.querySelector(
+        `[data-id=${dataIds.buttonOpenModalRegistrationService}]`
+    )
+    const $buttonCloseModalRegistrationService = document.querySelector(
+        `[data-id=${dataIds.buttonCloseModalRegistrationService}]`
+    )
+
+    const open = () => $modal.classList.remove('gone')
+
+    const close = () => $modal.classList.add('gone')
+
+    function listeningClickButtonOpenModalRegistrationService() {
+        $buttonOpenModalRegistrationService.addEventListener('click', () =>
+            open()
+        )
+    }
+
+    function listeningCloseButtonModalRegistrationService() {
+        $buttonCloseModalRegistrationService.addEventListener('click', () =>
+            close()
+        )
+    }
 
     return {
-        open: () => {
-            $modal.classList.remove('gone')
-        },
+        listeningEventsClicks: () => {
+            listeningClickButtonOpenModalRegistrationService()
 
-        close: () => {
-            $modal.classList.add('gone')
+            listeningCloseButtonModalRegistrationService()
         },
     }
 }
